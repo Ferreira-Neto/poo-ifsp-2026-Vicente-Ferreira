67d3beb19f7a01b320dc1c70370f14af8524b7d2
Ferreira-Neto <ferreira.neto@aluno.ifsp.edu.br>
2026-09-15 10:47:00 -0300

    Refactor main method in Teste class
    Updated main method to change name and age handling.

--- aula-04/entidades/Teste.java
+++ aula-04/entidades/Teste.java
@@ -1,18 +1,18 @@
 package aula04.entidades;
 
 public class Teste {
-
-    public static void main(String[] args) {
-
+    public static void main(String [] args){
         Pessoa p1 = new Pessoa();
         //p1.nome = "Ricardo";
-        String nome = "Ricardo";
+        String nome = "ricardo pessoa";
         p1.altereSeuNomePara(nome);
+        p1.alterarIdade(30);
 
-        //p1.idade = "-30";
 
-        p1.alterarIdade(-30);
 
+        System.out.println(p1.qualSeuNome());
+        System.out.println(p1.qualSuaIdade());
+        System.out.println(p1.qualUltimoNome());
     }
-
 }
+
