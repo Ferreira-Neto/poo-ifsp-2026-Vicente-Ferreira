7c43c3b3db46e60e430d84972f17945e2454b1c8
Ferreira-Neto <ferreira.neto@aluno.ifsp.edu.br>
2026-09-15 10:45:43 -0300

    Refactor Pessoa class methods and string handling

--- aula-04/entidades/Pessoa.java
+++ aula-04/entidades/Pessoa.java
@@ -1,38 +1,40 @@
-package aula04.entidades;
-
 public class Pessoa {
-
     private String nome;
     private int idade;
     private String ultimoNome;
 
-    private void dividirString(String texto) {
-        String[]
 
+    private void dividirString(String texto){
+        String [] nomes = texto.split(" ");
+        nome = nomes[0];
+        ultimoNome = nomes[1];
     }
-    public void altereSeuNomePara(String novoNome) {
 
-        if (novoNome == null || novoNome.isBlank()) {
-
+    public void altereSeuNomePara(String novoNome){
+        if (novoNome == null || novoNome.isBlank()){
             //System.err.println("Campo novoNome vazio ou nulo");
             //return;
-
             throw new IllegalArgumentException("Campo novoNome vazio ou nulo");
-
         }
+        dividirString(novoNome);
+    }
 
-        nome = novoNome;
-
-        public void alterarIdade(int i) {
-
-            if ( i >= 0 && i <= 120 ) {
-                idade = i;
-                return idade;
-            }
+    public void alterarIdade(int i){
+        if (i >= 0 && i <= 120){
+            idade = i;
         }
+    }
 
-        public int qualSuaIdade
+    public String qualSeuNome(){
+        return nome.toUpperCase();
+    }
 
+    public int qualSuaIdade(){
+        return idade;
+    }
+
+    public String qualUltimoNome(){
+        return ultimoNome;
     }
 
 }
